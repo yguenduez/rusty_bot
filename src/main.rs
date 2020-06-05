798a6ad768a94a6d4d1e49aaa79c2adb62d03241
ysngndz <ysngndz@gmail.com>
2020-06-05 17:48:10 +0200

    motor example - not wokring though

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,66 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, OutputPin};
 use rppal::system::DeviceInfo;
+
+struct Motor {
+    enable_pin: OutputPin,
+    pin_1: OutputPin,
+    pin_2: OutputPin,
+}
+
+impl Motor {
+    fn new(enable: u8, pin_1: u8, pin_2: u8) -> Result<Motor, rppal::gpio::Error> {
+        let enable_pin = Gpio::new()?.get(enable)?.into_output();
+        let pin_1 = Gpio::new()?.get(pin_1)?.into_output();
+        let pin_2 = Gpio::new()?.get(pin_2)?.into_output();
+
+        Ok(Motor {
+            enable_pin: enable_pin,
+            pin_1: pin_1,
+            pin_2: pin_2,
+        })
+    }
+    fn forward(&mut self) {
+        self.enable_pin.set_high();
+        self.pin_2.set_low();
+        self.pin_1.set_high();
+    }
+    fn backward(&mut self) {
+        self.enable_pin.set_high();
+        self.pin_1.set_low();
+        self.pin_2.set_high();
+    }
+    fn stop(&mut self) {
+        self.enable_pin.set_low();
+        self.pin_1.set_low();
+        self.pin_2.set_low();
+    }
+}
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_LED: u8 = 23;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    println!("Lets run a dc motor with h-bridge");
 
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    let mut motor = Motor::new(23, 24, 25)?;
 
-    // Blink the LED by setting the pin's logic level high for 500 ms.
-    let mut time = 0;
+    let mut times = 0;
     loop {
-        pin.set_high();
-        thread::sleep(Duration::from_millis(500));
-        pin.set_low();
-        thread::sleep(Duration::from_millis(500));
+        motor.stop();
+        thread::sleep_ms(500);
+        motor.forward();
+        thread::sleep_ms(3000);
+        motor.backward();
+        thread::sleep_ms(3000);
 
-        time += 1;
-        if time >= 10 {
+        if (times >= 10) {
             break;
         }
+        times += 1;
     }
+
     Ok(())
 }
