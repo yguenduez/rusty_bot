40ff9c13ad53555590424769e0b1ec28f742f2a1
ysngndz <ysngndz@gmail.com>
2020-06-09 21:58:23 +0200

    add working server

--- src/robot.rs
+++ src/robot.rs
@@ -1,6 +1,8 @@
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::system::DeviceInfo;
 
+use crate::tcp_server::{TCPServer};
+
 struct Motor {
     enable_pin: OutputPin,
     pin_1: OutputPin,
@@ -12,6 +14,7 @@ impl Motor {
         let enable_pin = Gpio::new()?.get(enable)?.into_output();
         let pin_1 = Gpio::new()?.get(pin_1)?.into_output();
         let pin_2 = Gpio::new()?.get(pin_2)?.into_output();
+
 
         Ok(Motor {
             enable_pin: enable_pin,
@@ -75,5 +78,26 @@ impl Robot {
     pub fn right(&mut self) {
         self.motor_1.backward();
         self.motor_2.forward();
+    }
+}
+
+struct RobotController{
+    tcp_server: TCPServer,
+    robot: Robot
+}
+
+impl RobotController {
+    pub fn new(robot: Robot)->RobotController{
+        RobotController{
+            robot: robot,
+            tcp_server: TCPServer::new()
+        }
+    }
+
+    pub fn run(&self){
+        self.tcp_server.start_listening();
+        loop {
+            
+        }
     }
 }
--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,40 +1,36 @@
+use std::fmt;
 use std::io::{stdin, BufRead, BufReader, Error, Write};
 use std::net::{Ipv4Addr, SocketAddr};
-use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
+use std::net::{TcpListener, TcpStream};
 
+use serde::Deserialize;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Point3D {
-    x: u32,
-    y: u32,
-    z: u32,
+struct Direction {
+    x: i32,
+    y: i32,
 }
 
-struct BindAdress {
-    ip: Ipv4Addr,
-    port: u32,
-}
-
-struct TCPServer {
+pub struct TCPServer {
     listener: TcpListener,
-    bind_adress: BindAdress,
+    buffer: Vec<Direction>
 }
 
 impl TCPServer {
-    fn new(bind_adress: BindAdress) -> TCPServer {
+    pub fn new() -> TCPServer {
         TCPServer {
-            bind_adress: bind_adress,
-            listener: TcpListener::bind("0.0.0.0:3333").unwrap(),
+            listener: TcpListener::bind("0.0.0.0:8888").unwrap(),
+            buffer: vec![]
         }
     }
-    fn run (&self) {
+    pub fn start_listening(&self) {
         for stream in self.listener.incoming() {
             match stream {
-                Err(e) => eprintln!("failed: {}", e),
+                Err(e) => eprintln!("Failed to connect: {}", e),
                 Ok(stream) => {
                     thread::spawn(move || {
-                        handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                        read_from_stream(stream).unwrap();
                     });
                 }
             }
@@ -42,45 +38,9 @@ impl TCPServer {
     }
 }
 
-fn handle_client(stream: TcpStream) -> Result<(), Error> {
-    println!("Incoming connection from: {}", stream.peer_addr()?);
-    let mut data = Vec::new();
-    let mut stream = BufReader::new(stream);
+fn read_from_stream(stream: TcpStream) -> Result<Direction, Error> {
+    let mut de = serde_json::Deserializer::from_reader(stream);
+    let u = Direction::deserialize(&mut de)?;
 
-    loop {
-        data.clear();
-
-        let bytes_read = stream.read_until(b'\n', &mut data)?;
-        if bytes_read == 0 {
-            return Ok(());
-        }
-        let input: Point3D = serde_json::from_slice(&data)?;
-        let value = input.x.pow(2) + input.y.pow(2) + input.z.pow(2);
-
-        write!(stream.get_mut(), "{}", f64::from(value).sqrt())?;
-        write!(stream.get_mut(), "{}", "\n")?;
-    }
-}
-
-fn run() {
-    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 3333");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
+    Ok(u)
 }
