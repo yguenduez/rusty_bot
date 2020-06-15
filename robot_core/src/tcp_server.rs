f47e22fbdd66e124874d3baa4549b3af42766615
ysngndz <ysngndz@gmail.com>
2020-06-15 19:32:28 +0200

    tmp threaded tcp server with arc mutex buffer

--- robot_core/src/tcp_server.rs
+++ robot_core/src/tcp_server.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use std::io::{stdin, BufRead, BufReader, Error, Write};
 use std::net::{Ipv4Addr, SocketAddr};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 #[derive(Serialize, Deserialize)]
@@ -20,12 +21,26 @@ impl Direction {
 
 pub struct TCPServer {
     pub listener: TcpListener,
+    handle: Option<thread::JoinHandle<()>>,
 }
 
 impl TCPServer {
     pub fn new() -> TCPServer {
         TCPServer {
             listener: TcpListener::bind("raspberrypi.local:3333").unwrap(),
+            handle: None,
+        }
+    }
+    pub fn start(&mut self) {
+        let stream_r = self.listener.accept();
+        match stream_r {
+            Ok((stream, _)) => {
+                self.handle = Some(thread::spawn(move || loop {
+                    let mut de = serde_json::Deserializer::from_reader(&stream);
+                    let u = Direction::deserialize(&mut de);
+                }));
+            }
+            Err(e) => println!("Could not connect! Error: {}", e),
         }
     }
 }
